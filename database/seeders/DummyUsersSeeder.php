6ff955348a1464856228aea23c7613b3cd593e68
Stevenc20 <stevenchristian2005@gmail.com>
2026-08-12 08:57:05 +0700

    Add DummyUsersSeeder to generate all test roles

---
+++ database/seeders/DummyUsersSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DummyUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            ['name' => 'Super Admin', 'email' => 'super@smkn40.id', 'password' => 'password', 'role' => 'super_admin', 'account_status' => 'active'],
+            ['name' => 'Admin System', 'email' => 'admin@smkn40.id', 'password' => 'password', 'role' => 'admin', 'account_status' => 'active'],
+            ['name' => 'Teacher', 'email' => 'teacher@smkn40.id', 'password' => 'password', 'role' => 'teacher', 'account_status' => 'active'],
+            ['name' => 'Student', 'email' => 'student@smkn40.id', 'password' => 'password', 'role' => 'student', 'account_status' => 'active'],
+        ];
+
+        foreach ($users as $user) {
+            \App\Models\User::updateOrCreate(
+                ['email' => $user['email']],
+                $user
+            );
+        }
+    }
+}
